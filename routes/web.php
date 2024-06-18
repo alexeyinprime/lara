0684dac12d6d001c9549bc6d6309a51ff4111eb9
Dachnik <dacha@local.host>
2024-06-19 00:59:41 +0300

    Add SoftStudio design and routing

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+})->name('home');
+Route::get('/index.html', function () {
+    return view('welcome');
+});
+
+Route::get('/blog.html', function () {
+    return view('blog');
+})->name('blog');
+
+Route::get('/contact.html', function () {
+    return view('contact');
+})->name('contact');
+
+Route::get('/blog-single.html', function () {
+    return view('post');
 });
